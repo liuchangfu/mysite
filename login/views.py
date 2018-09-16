b6fe70b8f99262aafaa490d27b43a5f8ef679993
Liuchangfu <shift_1220@163.com>
2018-09-16 21:34:48 +0800

    修改:增加session

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from . import models
+from . import forms
 
 
 # Create your views here.
@@ -11,22 +12,32 @@ def index(request):
 
 
 def login(request):
+    if request.session.get('is_login', None):
+        return redirect('/index/')
     if request.method == "POST":
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        if username and password:
-            username = username.strip()
-            print(username, password)
+        login_form = forms.UserForm(request.POST)
+        message = '所有的字段都必须填写！'
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
             try:
                 user = models.User.objects.get(name=username)
-                if user.password == password:
-                    return redirect('/index/')
-                else:
-                    message = '密码不正确!'
             except:
-                message = '用户名不存在!'
-        return render(request, 'login/login.html', {'message': message})
-    return render(request, 'login/login.html')
+                message = '用户不存在！'
+                return render(request, 'login/login.html', locals())
+            if user.password == password:
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
+                return redirect('/index/')
+            else:
+                message = '密码不正确!'
+                return render(request, 'login/login.html', locals())
+        else:
+            return render(request, 'login/login.html', locals())
+
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
 
 
 def register(request):
@@ -35,5 +46,7 @@ def register(request):
 
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        return redirect('/index/')
+    request.session.flush()
     return redirect('/index/')
--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login.apps.LoginConfig',
+    'captcha',
 ]
 
 MIDDLEWARE = [
