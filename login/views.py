5297b56269db3f03e76fd89d9afae87494be7d3b
Liuchangfu <shift_1220@163.com>
2018-09-16 17:31:10 +0800

    修改：index.html,login.html，views.py文件

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from . import models
 
 
 # Create your views here.
@@ -10,7 +11,21 @@ def index(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        if username and password:
+            username = username.strip()
+            print(username, password)
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = '密码不正确!'
+            except:
+                message = '用户名不存在!'
+        return render(request, 'login/login.html', {'message': message})
     return render(request, 'login/login.html')
 
 
